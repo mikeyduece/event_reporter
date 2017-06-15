ec5fabf10715e2e63b7b363aca0eb148999bff2b
Mike Heft <mikeheft@gmail.com>
2017-06-15 12:07:51 -0600

    help work

--- lib/event_message.rb
+++ lib/event_message.rb
@@ -1,10 +1,10 @@
 module EventMessages
   def queue_help(command=nil)
-    puts "The queue holds the stored search results form previous searches.
-    It is not cleared until the user enters the queue clear command or starts
-    a new seach.
-      The queue related commands are: count, clear, print, print_by<attribute>,
-      and print_by<filename.csv>"
+    puts "The queue holds the stored search results form previous searches.\n
+    It is not cleared until the user enters the queue clear command or starts\n
+    a new seach.\n
+      \tThe queue related commands are: count, clear, print,\n
+      \tprint_by<attribute>, and print_by<filename.csv>"
   end
 
   def attribute_help
@@ -25,5 +25,11 @@ module EventMessages
     puts "The find command searches the attendees matching your search criteria.
       Search format is as follows
       find(attribute, criteria)."
+  end
+
+  def print_help
+    puts "Print out a tab-delimited data table with a header row following this format:
+
+      LAST NAME  FIRST NAME  EMAIL  ZIPCODE  CITY  STATE  ADDRESS  PHONE  DISTRICT"
   end
 end
