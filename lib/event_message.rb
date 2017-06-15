4d90ab640adf7e7ec98da75ee3781c0ebe6516fc
Mike Heft <mikeheft@gmail.com>
2017-06-15 11:46:29 -0600

    work on help commands

--- lib/event_message.rb
+++ lib/event_message.rb
@@ -1,5 +1,29 @@
 module EventMessages
-  def help(command=nil)
-    puts ""
+  def queue_help(command=nil)
+    puts "The queue holds the stored search results form previous searches.
+    It is not cleared until the user enters the queue clear command or starts
+    a new seach.
+      The queue related commands are: count, clear, print, print_by<attribute>,
+      and print_by<filename.csv>"
+  end
+
+  def attribute_help
+    "The criteria you can search for are:
+      - last_name
+      - first_name
+      - email
+      - zipcode
+      - city
+      - state
+      - street
+      - city
+      - state
+      - home_phone"
+  end
+
+  def find_help
+    puts "The find command searches the attendees matching your search criteria.
+      Search format is as follows
+      find(attribute, criteria)."
   end
 end
